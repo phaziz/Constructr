b3f2ba0c10628093d40c7d093a52c03dad507bb5
Christian Becher <cbecher@stressless-it.de>
2014-06-16 16:24:51 +0200

    Bugfixing.

--- Uploads/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-    
-    if(realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])) header('Location: ../login'); die();
--- Web/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    echo '<center><h1>Start here to build your own frontend (./root/Web/index.php) based on ConstructrCMS Database Structure :)</h1>';
-    echo 'Your _EXT_WWW != "" -> defined in ./root/index.php';
-    echo '</center><br><br>';
-
-?>

5dbc8e369a88ad3bcf900c17b249c255952a09eb
Christian Becher <office@phaziz.com>
2014-08-25 21:37:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -22,8 +22,8 @@
     require_once './Config/constructr.conf.php';
     require_once './Config/constructr_user_rights.conf.php';
 
-    $_CONSTRUCTR_CONF['_VERSION_DATE'] = '20140814';
-    $_CONSTRUCTR_CONF['_VERSION'] = '1.03.1';
+    $_CONSTRUCTR_CONF['_VERSION_DATE'] = '20140825';
+    $_CONSTRUCTR_CONF['_VERSION'] = '1.03.2';
 
     require_once './Slim/Slim.php';
     require_once './Slim/Log/DateTimeFileWriter.php';
